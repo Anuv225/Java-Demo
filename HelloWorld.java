a67e007c3b8311375900e995c5402b37d8b81092
Anuv225 <anuvindpl05@gmail.com>
2024-10-17 09:12:55 +0530

    Print changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ class HelloWorld
 {
     public static void main(String []args)
     {
-        System.out.println("Anuvinds first java code");
+        System.out.println("Anuvinds first java code in vscod with git demo");
     }
 };
