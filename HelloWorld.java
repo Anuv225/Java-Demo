e0c9371b8ad7175a352613b71e36391e87d88821
Anuv225 <anuvindpl05@gmail.com>
2024-10-18 12:01:04 +0530

    Sum of three Numbers

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
 class HelloWorld
 {
     public static void main(String []args)
     {
-        System.out.println("Anuvinds first java code in vscod with git demo");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 numbers");
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int min_num = Math.min(a,b);
+        min_num = Math.min(min_num,c);
+
+        int max_num = Math.max(a,b);
+        max_num = Math.max(max_num,c);
+
+        int sum_nums = max_num + min_num;
+
+        System.out.println("Sum of number is " + sum_nums);
+
+
     }
 };
